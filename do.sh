bcb616eea0e2464700c2f56daa3c757b9ba55e5e
FengodChen <a1176553293@outlook.com>
2019-04-02 01:16:55 +0800

    change the name binary output file

--- do.sh
+++ do.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-g++ main.cpp include/fengod_compare.cpp -o main -L /usr/local/lib -lopencv_core -lopencv_imgproc -lopencv_videoio -lopencv_highgui -lopencv_video -lopencv_imgcodecs
+g++ main.cpp include/fengod_compare.cpp -o run -L /usr/local/lib -lopencv_core -lopencv_imgproc -lopencv_videoio -lopencv_highgui -lopencv_video -lopencv_imgcodecs
